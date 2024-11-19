9ad146549e945db31d500ff6a04d5a7db5bb7325
Valentina Heredia <valu.heredia05@gmail.com>
2024-11-19 19:37:18 -0300

    Se agregaron rutas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Edificios from './Componentes/Empleados/PagEdificios/Edificios';
 import Unidades from './Componentes/Empleados/PagUnidades/Unidades';
 import { AuthProvider } from './Componentes/AuthContext';
 import ProtectedRoute from './Componentes/ProtectedRoute';
+import Personas from './Componentes/Empleados/PagPersonas/Personas';
+import Reclamos from './Componentes/Empleados/PagReclamos/Reclamos';
+import Usuario from './Componentes/Empleados/PagUsuario/Usuario';
+import Empleado from './Componentes/Empleados/PagEmpleados/Empleado';
 
 function App() {
   return (
@@ -32,7 +36,10 @@ function App() {
           >
             <Route path="Edificios" element={<Edificios />} />
             <Route path="Unidades" element={<Unidades />} />
-            {/* Otras rutas protegidas */}
+            <Route path="Reclamos" element={<Reclamos />} />
+            <Route path="Personas" element={<Personas />} />
+            <Route path="Usuarios" element={<Usuario />} />
+            <Route path="Empleados" element={<Empleado />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/Componentes/EmpleadosV.jsx
+++ src/Componentes/EmpleadosV.jsx
@@ -4,16 +4,16 @@ export default function EmpleadosV() {
     return (
     <div className="fondo">
         <nav>
-        <ul>
-            <li><Link to="Edificios">Edificios</Link></li>
-            <li><Link to="Unidades">Unidades</Link></li>
-            <li><Link to="Reclamos">Reclamos</Link></li>
-            <li><Link to="Personas">Personas</Link></li>
-            <li><Link to="Usuarios">Usuarios</Link></li>
-            <li><Link to="Empleados">Empleados</Link></li>
-        </ul>
+            <ul>
+                <li><Link to="Edificios">Edificios</Link></li>
+                <li><Link to="Unidades">Unidades</Link></li>
+                <li><Link to="Reclamos">Reclamos</Link></li>
+                <li><Link to="Personas">Personas</Link></li>
+                <li><Link to="Usuarios">Usuarios</Link></li>
+                <li><Link to="Empleados">Empleados</Link></li>
+            </ul>
         </nav>
-        <Outlet /> {/* Aquí se renderizarán los componentes de las rutas hijas */}
+        <Outlet />
     </div>
     );
 }
